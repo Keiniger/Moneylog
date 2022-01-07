6c54fc0214c5fe1497cf9c3fa432342c5c95f1ae
Nacho <nkeiniger@gmail.com>
2022-01-07 20:13:21 -0300

    last font change

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -26,7 +26,7 @@ const NewExpense = (props) => {
   return (
     <div className='new-expense'>
       {!isEditing && (
-        <button onClick={startEditingHandler}>Add New Expense</button>
+        <button onClick={startEditingHandler}>Add new expense</button>
       )}
       {isEditing && (
         <ExpenseForm
